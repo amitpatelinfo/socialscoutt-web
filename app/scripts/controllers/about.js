1c6ebb753706d15bab42437564e8712c104abb85
Amit  Patel <amitpatelinfo@gmail.com>
2014-12-26 11:00:07 +0530

    Adding application structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name socialscouttWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the socialscouttWebApp
+ */
+angular.module('socialscouttWebApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
